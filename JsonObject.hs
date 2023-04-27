1caec0931a6f583ea9b25f631708f37103ca918d
cristopher7w7 <cristopher.miranda.58909@usalesiana.edu.bo>
2023-04-26 21:17:48 -0400

    json object added

---
+++ JsonObject.hs
@@ -0,0 +1,10 @@
+module JsonObject(JsonValue(..),NodeName) where
+
+type NodeName = String
+data JsonValue = JString String 
+            | JBool Bool 
+            | JObject [(String, Maybe JsonValue)]
+            | JNumber Double 
+            | JList [JsonValue]
+            | JNil
+            deriving Show
